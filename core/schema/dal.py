90e861fdaa65aed172277e7d1ae98e26de078f97
eddyizm <eddyizm@gmail.com>
2023-02-25 14:16:31 -0800

    updated quote staging table

--- core/schema/dal.py
+++ core/schema/dal.py
@@ -1,6 +1,9 @@
 import sqlalchemy
+from datetime import datetime
 from databases import Database
 from sqlalchemy.sql.sqltypes import Boolean
+from sqlalchemy.sql import func, expression
+
 DATABASE_URL = 'sqlite:///./core/schema/quotes_app.sqlite3'
 
 database = Database(DATABASE_URL)
@@ -28,8 +31,7 @@ quotes = sqlalchemy.Table(
     sqlalchemy.Column('category', sqlalchemy.String(25)),
     sqlalchemy.Column('quote', sqlalchemy.String(2000)),
     sqlalchemy.Column('author', sqlalchemy.String(50)),
-    sqlalchemy.Column('date_added', sqlalchemy.DATE(), 
-    server_default=sqlalchemy.sql.expression.false(), nullable=False)
+    sqlalchemy.Column('date_added', sqlalchemy.DATE(), default=datetime.now, nullable=False)
     )
 
 quotes_staging = sqlalchemy.Table(
@@ -40,10 +42,9 @@ quotes_staging = sqlalchemy.Table(
     sqlalchemy.Column('category', sqlalchemy.String(25)),
     sqlalchemy.Column('quote', sqlalchemy.String(2000)),
     sqlalchemy.Column('author', sqlalchemy.String(50)),
-    sqlalchemy.Column('added_to_quotes', sqlalchemy.BOOLEAN),
+    sqlalchemy.Column('added_to_quotes', sqlalchemy.BOOLEAN, server_default=expression.false()),
     sqlalchemy.Column('added_by', sqlalchemy.String(50)),
-    sqlalchemy.Column('date_created', sqlalchemy.DATE(),
-   server_default=sqlalchemy.sql.expression.false(), nullable=False)
+    sqlalchemy.Column('date_created', sqlalchemy.DATE(), server_default=func.now())
     )
 
 quote_history = sqlalchemy.Table(
