1fad3606fa6b528f9ccd399a2e555f1be0c97c9d
eddyizm <eddyizm@gmail.com>
2023-02-25 14:16:51 -0800

    added staging model

--- core/models/quote_models.py
+++ core/models/quote_models.py
@@ -1,4 +1,5 @@
 from pydantic import BaseModel
+from typing import Optional
 
 
 class Quote(BaseModel):
@@ -13,3 +14,11 @@ class Category(BaseModel):
 
 class Author(BaseModel):
     author: str
+
+
+class Quote_Staging(BaseModel):
+    quote: str
+    author: str
+    category: str
+    added_to_quotes: Optional[bool]
+    added_by: str
