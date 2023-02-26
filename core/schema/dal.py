e5375d7aadefcc61908ddd77d463c7d472c27438
eddyizm <eddyizm@gmail.com>
2023-02-25 17:15:08 -0800

    fixed database path error

--- core/schema/dal.py
+++ core/schema/dal.py
@@ -4,7 +4,7 @@ from databases import Database
 from sqlalchemy.sql.sqltypes import Boolean
 from sqlalchemy.sql import func, expression
 
-DATABASE_URL = 'sqlite:///./core/schema/    quotes_app.sqlite3'
+DATABASE_URL = 'sqlite:///./core/schema/quotes_app.sqlite3'
 
 database = Database(DATABASE_URL)
 metadata = sqlalchemy.MetaData()
