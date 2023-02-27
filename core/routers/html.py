804c167073508546efcc52ec764c6c1d5da10dbd
eddyizm <eddyizm@gmail.com>
2023-02-26 21:26:50 -0800

    added paper css to style contruction page

--- core/routers/html.py
+++ core/routers/html.py
@@ -2,11 +2,13 @@ from fastapi import APIRouter, Request, status, Response
 from fastapi.responses import HTMLResponse
 from fastapi.templating import Jinja2Templates
 
+from core.config import settings
+
 templates = Jinja2Templates(directory="html/templates")
 router = APIRouter()
 
 
 @router.get("/" ,response_class=HTMLResponse,  status_code=status.HTTP_200_OK)
 async def home(response: Response, request:Request): 
-    response = templates.TemplateResponse("base.html", {"request": request})
+    response = templates.TemplateResponse("base.html", {"request": request, "site_title": settings.SITE_TITLE })
     return response
--- main.py
+++ main.py
@@ -1,8 +1,10 @@
 from fastapi import FastAPI
+from fastapi.staticfiles import StaticFiles
 
 from core.routers import html, quotes_api
 
 app = FastAPI()
+app.mount("/html/static", StaticFiles(directory="html/static"), name="static")
 
 app.include_router(html.router)
 app.include_router(quotes_api.router)
