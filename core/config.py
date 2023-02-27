dbfb162ae571da372af27ac2d38a0d35a14457f8
eddyizm <eddyizm@gmail.com>
2023-02-26 21:24:41 -0800

    added settings class

--- core/config.py
+++ core/config.py
@@ -3,3 +3,9 @@ from dotenv import load_dotenv
 
 dotenv_path = os.path.join(os.path.dirname(__file__), '.env')
 load_dotenv(dotenv_path)
+
+class Settings:
+    SITE_TITLE = os.getenv('SITE_TITLE')
+
+
+settings = Settings()
