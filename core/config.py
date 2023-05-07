7475b464538fe40cf38836210c9e87b81fe65e25
eddyizm <eddyizm@gmail.com>
2023-05-06 17:19:21 -0700

    updated settings for jwt auth. Started setting up a cache for this #9 #11

--- core/config.py
+++ core/config.py
@@ -1,11 +1,17 @@
 import os
 from dotenv import load_dotenv
+from functools import cache
 
 dotenv_path = os.path.join(os.path.dirname(__file__), '.env')
 load_dotenv(dotenv_path)
 
+@cache
 class Settings:
     SITE_TITLE = os.getenv('SITE_TITLE')
+    VERSION = os.getenv('VERSION')
+    SECRET_KEY = os.getenv('SECRET_KEY')
+    ALGORITHM = 'HS256'
+    ACCESS_TOKEN_EXPIRE_MINUTES=60
 
 
 settings = Settings()
