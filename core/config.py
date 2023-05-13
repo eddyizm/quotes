51c9dfd5f380e3d58bcb3a84328004b0823f976a
eddyizm <eddyizm@gmail.com>
2023-05-13 13:38:39 -0700

    removed cache from settings

--- core/config.py
+++ core/config.py
@@ -1,11 +1,9 @@
 import os
 from dotenv import load_dotenv
-from functools import cache
 
 dotenv_path = os.path.join(os.path.dirname(__file__), '.env')
 load_dotenv(dotenv_path)
 
-@cache
 class Settings:
     SITE_TITLE = os.getenv('SITE_TITLE')
     VERSION = os.getenv('VERSION')
