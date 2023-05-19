3fc4a90ea3b53be32ccc77a078fedaf426eae981
eddyizm <eddyizm@gmail.com>
2023-05-18 21:59:40 -0700

    printing out test quote

--- tests/test_quotes.py
+++ tests/test_quotes.py
@@ -5,5 +5,7 @@ async def test_daily_q():
     res = await quote.daily_quote()
     assert isinstance(res[0], int)
 
+
 async def test_approve_new_quote():
     res = await quote.approve_new_quote(1)
+    print(res)
