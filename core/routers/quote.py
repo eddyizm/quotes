bfd3c5e4f3b988d8df4ef4dcb10b17b09cd65b42
eddyizm <eddyizm@gmail.com>
2023-05-22 21:40:41 -0700

    refactored getting unapproved quote

--- core/routers/quote.py
+++ core/routers/quote.py
@@ -1,6 +1,6 @@
 '''Shared code'''
 from datetime import datetime
-from sqlalchemy import select
+from sqlalchemy import select, exc
 from core.models.quote_models import Quote, Quote_Staging
 from core.schema.sql_views import RANDOM_QUOTE
 from core.schema.dal import quotes, quote_history, quotes_staging, database
@@ -33,10 +33,18 @@ async def submit_new_quote(new_quote: Quote_Staging):
     return await database.execute(query)
 
 
-async def approve_new_quote(quote_id: int):
-    query = quotes_staging.select().where(quotes_staging.c.added_to_quotes == 0).\
+async def get_submitted_quote_by_id(quote_id) -> Quote:
+    """ Get submitted quote from staging table that has not yet been approved"""
+    try:
+        query = quotes_staging.select().where(quotes_staging.c.added_to_quotes == 0).\
         where(quotes_staging.c.id == quote_id)
-    new_quote = await database.fetch_one(query)
+        return await database.fetch_one(query)
+    except exc.NoResultFound as ex:
+        raise ex
+
+
+async def approve_new_quote(quote_id: int):
+    new_quote = get_submitted_quote_by_id(quote_id)
     insert_query =  quotes.insert().values(
             quote = new_quote.quote,
             author = new_quote.author,
