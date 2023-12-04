d23e1d969d0b15cad2f5da458d15c0772f4bad08
eddyizm <eddyizm@gmail.com>
2023-12-03 19:08:54 -0800

    formatted

--- core/config.py
+++ core/config.py
@@ -3,38 +3,41 @@ import os
 from dotenv import load_dotenv
 from urllib.parse import quote
 
-dotenv_path = os.path.join(os.path.dirname(__file__), '.env')
+dotenv_path = os.path.join(os.path.dirname(__file__), ".env")
 load_dotenv(dotenv_path)
 year = datetime.now().year
 
-class Settings:
-    SITE_TITLE = os.getenv('SITE_TITLE')
-    VERSION = os.getenv('VERSION')
-    SECRET_KEY = os.getenv('SECRET_KEY')
-    ALGORITHM = 'HS256'
-    ACCESS_TOKEN_EXPIRE_MINUTES = int(os.getenv('ACCESS_TOKEN_EXPIRE_MINUTES'))
-    YEAR=year
-    POSTGRES_SERVER=os.getenv('POSTGRES_SERVER')
-    POSTGRES_USER=os.getenv('POSTGRES_USER')
-    POSTGRES_PASSWORD=quote(os.getenv('POSTGRES_PASSWORD'))
-    POSTGRES_DB=os.getenv('POSTGRES_DB')
-    POSTGRES_PORT=os.getenv('POSTGRES_PORT')
 
+class Settings:
+    SITE_TITLE = os.getenv("SITE_TITLE")
+    VERSION = os.getenv("VERSION")
+    SECRET_KEY = os.getenv("SECRET_KEY")
+    ALGORITHM = "HS256"
+    ACCESS_TOKEN_EXPIRE_MINUTES = int(os.getenv("ACCESS_TOKEN_EXPIRE_MINUTES"))
+    YEAR = year
+    POSTGRES_SERVER = os.getenv("POSTGRES_SERVER")
+    POSTGRES_USER = os.getenv("POSTGRES_USER")
+    POSTGRES_PASSWORD = quote(os.getenv("POSTGRES_PASSWORD"))
+    POSTGRES_DB = os.getenv("POSTGRES_DB")
+    POSTGRES_PORT = os.getenv("POSTGRES_PORT")
 
     def quote_response(self, request, quote) -> dict:
-        return {"request": request, 
-        "site_title": settings.SITE_TITLE,
-        "year": settings.YEAR,
-        "version": settings.VERSION,
-        'page_title': 'Daily Quote!',
-        "daily_quote": quote }
-
-    def non_quote_response(self, request) -> dict:
-        return {"request": request, 
+        return {
+            "request": request,
             "site_title": settings.SITE_TITLE,
             "year": settings.YEAR,
-            "version": settings.VERSION
-            }
+            "version": settings.VERSION,
+            "page_title": "Daily Quote!",
+            "daily_quote": quote,
+        }
+
+    def non_quote_response(self, request) -> dict:
+        return {
+            "request": request,
+            "site_title": settings.SITE_TITLE,
+            "year": settings.YEAR,
+            "version": settings.VERSION,
+        }
 
 
 settings = Settings()
