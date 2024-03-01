9fda31f94d5466685090da030a6f9ba843cba18e
eddyizm <eddyizm@gmail.com>
2024-02-29 21:43:05 -0800

    testing out caddy

--- start_pod.sh
+++ start_pod.sh
@@ -7,8 +7,16 @@ podman volume create letsencrypt
 echo "Create pod quote_pod"
 podman pod create -p 8080:80 -p 8081:443 --name=quote_pod
 
+echo "Starting quote pod..."
+podman pod start quote_pod 
+
 echo "Create nginx container in pod"
 podman run -d --pod=quote_pod -v letsencrypt:/etc/letsencrypt --name=reverse-proxy  reverse-proxy
+
+podman run -d --pod=quote_pod \
+    -v $PWD/Caddyfile:/etc/caddy/Caddyfile \
+    -v letsencrypt:/data \
+    docker.io/library/caddy:2.7.6-alpine
 
 echo "Spinning up postgres container"
 podman run -d --pod=quote_pod --name=postgres_db -v dbdata:/var/lib/postgresql/data  --env-file core/.env docker.io/postgres:latest
