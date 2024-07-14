04265b6de57d2dc5dffb82e87e0b40592f75e2f5
eddyizm <eddyizm@gmail.com>
2024-07-13 21:17:56 -0700

    feature: move auth user to security to share across app, updated utc

--- src/core/security.py
+++ src/core/security.py
@@ -1,17 +1,34 @@
 from fastapi.security import HTTPAuthorizationCredentials, HTTPBearer
 from fastapi import Security
 from passlib.context import CryptContext
-from datetime import datetime, timedelta
+from datetime import datetime, timedelta, timezone
 from typing import Any, Union
 from jose import jwt
-from src.core.config import settings
 
+from src.core.config import settings
+from src.core.models.user_models import User
+from src.core.schema.dal import database, users
 
 class AuthHandler():
     security = HTTPBearer()
     pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
     ALGORITHM = settings.ALGORITHM
 
+    async def authenticate_user(self, username, password):
+        try:
+            user = User(email=username,
+                        password=password)
+            query = users.select().where((users.c.email == user.email) & (users.c.is_active == True))
+            result = await database.fetch_one(query)
+            if result:
+                print('user found, check password')
+                password_check = self.verify_password(user.password, result[2])
+                print(f'password check result: {password_check}')
+                return password_check
+            else:
+                return False
+        except RequiresLoginException:
+            raise RequiresLoginException()
 
     def decode_token(self, token):
         try:
@@ -26,33 +43,29 @@ class AuthHandler():
             # raise HTTPException(status_code=401, detail='Invalid token')
         except Exception as e:
             raise RequiresLoginException()
-            
-    
+
     def auth_wrapper(self, auth: HTTPAuthorizationCredentials = Security(security)):
         return self.decode_token(auth.credentials)
 
-
-    def create_access_token(self,
+    def create_access_token(
+        self,
         subject: Union[str, Any], expires_delta: timedelta = None
     ) -> str:
         if expires_delta:
-            expire = datetime.utcnow() + expires_delta
+            expire = datetime.now(timezone.UTC) + expires_delta
         else:
-            expire = datetime.utcnow() + timedelta(
+            expire = datetime.now(timezone.UTC) + timedelta(
                 minutes=settings.ACCESS_TOKEN_EXPIRE_MINUTES
             )
         to_encode = {"exp": expire, "sub": str(subject)}
         encoded_jwt = jwt.encode(to_encode, settings.SECRET_KEY, algorithm=settings.ALGORITHM)
         return encoded_jwt
 
-
     def get_hash_password(self, plain_password):
         return self.pwd_context.hash(plain_password)
 
-
     def verify_password(self, plain_password, hash_password):
         return self.pwd_context.verify(plain_password, hash_password)
-
 
     def is_admin(self):
         # TODO -add admin role or scope
