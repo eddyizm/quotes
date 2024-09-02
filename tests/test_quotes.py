59bce41a6970c1856deb044e14c1a3567c956bbf
eddyizm <eddyizm@gmail.com>
2024-09-02 09:44:22 -0700

    wip: disabling tests to mock db

--- tests/test_quotes.py
+++ tests/test_quotes.py
@@ -1,21 +1,26 @@
-from core.routers import quote
-
-# TODO mock out the db
-async def test_daily_q():
-    res = await quote.daily_quote()
-    assert isinstance(res[0], int)
+# from fastapi.testclient import TestClient
+# from src.main import app
+# from src.core.routers import quote
 
 
-async def test_approve_new_quote():
-    res = await quote.approve_new_quote(1)
-    print(res)
+# client = TestClient(app)
+
+# # TODO mock out the db
+# async def test_daily_q():
+#     res = await client.daily_quote()
+#     assert isinstance(res[0], int)
 
 
-async def test_get_submitted_quote_by_id():
-    res = await quote.get_submitted_quote_by_id(1)
-    print(res)
+# async def test_approve_new_quote():
+#     res = await client.approve_new_quote(1)
+#     print(res)
 
 
-async def test_get_submitted_quote_by_id_not_found():
-    res = await quote.get_submitted_quote_by_id('test')
-    assert res is None
+# async def test_get_submitted_quote_by_id():
+#     res = await client.get_submitted_quote_by_id(1)
+#     print(res)
+
+
+# async def test_get_submitted_quote_by_id_not_found():
+#     res = await client.get_submitted_quote_by_id('test')
+#     assert res is None
